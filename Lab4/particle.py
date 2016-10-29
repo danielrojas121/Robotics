1da5ec370e8504f9db47acbc7edf6fe64b1b7ca7
Daniel Rojas <danielrojas@dyn-160-39-208-193.dyn.columbia.edu>
2016-10-29 18:01:25 -0400

    Added git ignore

--- Lab4/particle.py
+++ Lab4/particle.py
@@ -1,6 +1,19 @@
 import sys
+from turtle import *
 
-with open(sys.argv[1], 'r') as f:
-	for line in f:
-		for num in line.split():
-			print num
+
+def main():
+	if len(sys.argv) == 2:
+		readFile()
+	else:
+		print "Error: Must provide one coordinate text file as an argument"
+		sys.exit(1)
+
+
+def readFile():
+	with open(sys.argv[1], 'r') as f:
+		for line in f:
+			for num in line.split():
+				print num
+
+main()
