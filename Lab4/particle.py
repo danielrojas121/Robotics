757207cb81b3a783d94156522099789a43153e87
Daniel Rojas <danielrojas@dyn-160-39-208-193.dyn.columbia.edu>
2016-10-30 02:36:32 -0400

    Added draw world

--- Lab4/particle.py
+++ Lab4/particle.py
@@ -1,7 +1,6 @@
 import sys
 from turtle import *
 
-
 def main():
 	if len(sys.argv) == 2:
 		readFile()
@@ -12,8 +11,32 @@ def main():
 
 def readFile():
 	with open(sys.argv[1], 'r') as f:
+		world_x, world_y = [int(x) for x in next(f).split()]
+		drawWorld(world_x, world_y)
 		for line in f:
-			for num in line.split():
-				print num
+			x, y = [int(x) for x in next(f).split()]
+			#drawObstable(x, y)
+
+def drawWorld(x, y):
+	mode('standard')
+	color('red')
+	speed(0)
+	penup()
+	setposition(0,0)
+	pendown()
+	forward(x)
+	left(90)
+	forward(y)
+	left(90)
+	forward(x)
+	left(90)
+	forward(y)
+	#turn left to again be in horizontal X axis
+	left(90)
+	penup()
+	done()
+
+#def drawObstable(x, y):
+
 
 main()
