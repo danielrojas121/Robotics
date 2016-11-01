4f48c3179e87c393ca7b75ccdd7e1191ba6923c1
frankcabada <frankcabada@yahoo.com>
2016-11-01 18:08:17 -0400

    Implemented drawParticle method, began particle generation code

--- Lab4/particle.py
+++ Lab4/particle.py
@@ -1,56 +1,93 @@
 import sys
+import random
 from turtle import *
 
 mode('standard')
 color('red')
 speed(0)
+scale = 3
+offsetx = -scale/2 * 150
+offsety = -scale/2 * 150
+particle_count = 0
+object_list = []
+particle_list = []
 
 def main():
-	if len(sys.argv) == 2:
-		readFile()
+	if len(sys.argv) == 3:
+		drawInitialObjects()
 	else:
-		print "Error: Must provide one coordinate text file as an argument"
+		print "Error: Incorrect command line arguments"
+		print "Format: python particle.py <coordinates_filename> <particle_count>"
 		sys.exit(1)
 
 
-def readFile():
-	with open(sys.argv[1], 'r') as f:
+def drawInitialObjects():
+	global object_list, particle_count
+	filename = sys.argv[1]
+	with open(filename, 'r') as f:
 		world_x, world_y = [int(x) for x in next(f).split()]
 		drawWorld(world_x, world_y)
 		for line in f:
 			x, y = [float(x) for x in line.split()]
-			drawObstable(x, y)
+			drawObstacle(x, y)
+	particle_count = int(sys.argv[2])
+	for p in range(0, particle_count):
+		i = random.randint(0, world_x)
+		j = random.randint(0, world_y)
+		theta = random.randint(0, 359)
+		#iterate through obstacles to make sure particle position ok
+		#if ok, add to particle list here (remove from draw particle)
+		#if p%10==0 draw particle
+	print object_list
+	print particle_list
 	done()
 
 def drawWorld(x, y):
 	penup()
-	setposition(0,0)
+	setposition(offsetx, offsety)
 	pendown()
-	forward(x)
+	forward(scale*x)
 	left(90)
-	forward(y)
+	forward(scale*y)
 	left(90)
-	forward(x)
+	forward(scale*x)
 	left(90)
-	forward(y)
+	forward(scale*y)
 	#turn left to again be in horizontal X axis
 	left(90)
 	penup()
 
-def drawObstable(x, y):
+def drawObstacle(x, y):
+	global object_list
 	begin_fill()
 	penup()
-	setposition(x - 5.7, y - 5.7)
+	bottom_left = (x-5.7, y-5.7)
+	#keep track of bottom left corner of each obstacle
+	object_list.append(bottom_left)
+	setposition(offsetx + scale*bottom_left[0], offsety + scale*bottom_left[1])
 	pendown()
-	forward(11.4)
+	forward(scale*11.4)
 	left(90)
-	forward(11.4)
+	forward(scale*11.4)
 	left(90)
-	forward(11.4)
+	forward(scale*11.4)
 	left(90)
-	forward(11.4)
+	forward(scale*11.4)
 	#turn left to again be in horizontal X axis
 	left(90)
+	penup()
+	end_fill()
+
+def drawParticle(x, y, theta):
+	global particle_list
+	color('blue')
+	begin_fill()
+	penup()
+	particle = [x, y, theta] #move this and...
+	particle_list.append(particle) #move this outside to include ALL particles
+	setposition(offsetx + scale*x, offsety + scale*y)
+	pendown()
+	circle(1)
 	penup()
 	end_fill()
 
