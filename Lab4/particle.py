8cebdf3452b8cd9260930193c7a598fe0a8cb647
Stephanie Burgos <stephburgos96@gmail.com>
2016-11-05 19:10:21 -0400

    Creates sense function. Still a work in progress

--- Lab4/particle.py
+++ Lab4/particle.py
@@ -26,3 +26,28 @@ class Particle(object):
         self.x = self.position_matrix[0,2]
         self.y = self.position_matrix[1,2]
         return (self.x, self.y, self.theta)
+
+    def sense(self, dist):
+        dist_list = []
+        theta = 0
+        sense_dist = 0
+        while(theta <= 180):
+            theta_rad = math.radians(theta)
+            delta_x =  math.cos(theta_rad) * dist
+            delta_y = math.sin(theta_rad) * dist
+            x = self.x + delta_x
+            y = self.y + delta_y
+            
+	    #check obstacle list to determine if sensor detects them
+            for x in range(0, len(object_list)):
+                obstacle = object_list[x]
+		if (obstacle[0] < x) and (x < obstacle[0] + 11.4):
+		    if (obstacle[1] - BUFFER < y) and (y < obstacle[1] + 11.4):
+		        
+
+
+
+            #add sensor data to array and add to theta
+            theta += 20
+            dist_list.append(sense_dist)
+        return dist_list
