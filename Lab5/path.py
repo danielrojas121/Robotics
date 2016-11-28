6582ceee8c9f728414af21e4e40f6762f8de186e
Daniel Rojas <danielrojas@dyn-160-39-208-17.dyn.columbia.edu>
2016-11-28 12:19:06 -0500

    Added object list and object drawing

--- Lab5/path.py
+++ Lab5/path.py
@@ -7,13 +7,18 @@ speed(0)
 scale = 2
 offsetx = -scale/2 * 250
 offsety = -scale/2 * 250
+object_list = []
 
 def main():
 	if len(sys.argv) == 2:
 		filename = sys.argv[1]
 		f = open(filename, 'r')
 		read_file(f)
-	done()
+		done()
+	else:
+		print "Error: Incorrect command line arguments"
+		print "Format: python path.py <coordinates_filename>"
+		sys.exit(1)
 
 def read_file(infile):
 	start_x, start_y = [int(x) for x in infile.readline().split()]
@@ -24,19 +29,25 @@ def read_file(infile):
 	draw_circle(start_x, start_y)
 	draw_circle(goal_x, goal_y)
 
+	vertex_list = []
+
 	obstacle_count = int(infile.readline())
 	v_count = int(infile.readline())
 	line = infile.readline()
 	while (line != ""):
 		if v_count == 0:
 			v_count = int(line)
+			object_list.append(vertex_list)
+			vertex_list = []
 		else:
 			x, y = [float(x) for x in line.split()]
 			coordinate = (x,y)
+			vertex_list.append(coordinate)
 			v_count -= 1
-			print coordinate
 		line = infile.readline()
-		
+	object_list.append(vertex_list)
+	draw_objects()
+
 def draw_world(x, y):
 	penup()
 	setposition(offsetx, offsety)
@@ -63,26 +74,21 @@ def draw_circle(x, y):
 	#turn left to again be in horizontal X axis
 	penup()
 	end_fill()
-'''
-def draw_obstacle(x, y):
-	begin_fill()
-	penup()
-	#keep track of bottom left and top right corners of each obstacle
-	bottom_left = (x-5.7, y-5.7)
-	top_right = (x+5.7, y+5.7)
 
-	setposition(offsetx + scale*bottom_left[0], offsety + scale*bottom_left[1])
-	pendown()
-	forward(scale*11.4)
-	left(90)
-	forward(scale*11.4)
-	left(90)
-	forward(scale*11.4)
-	left(90)
-	forward(scale*11.4)
-	#turn left to again be in horizontal X axis
-	left(90)
+def draw_objects():
+	global object_list
+
 	penup()
-	end_fill()
-'''
+	i = 0
+	while(i<len(object_list)):
+		coordinates = object_list[i]
+		j = 0 
+		while(j<len(coordinates)):
+			setposition(coordinates[j][0] * scale + offsetx, coordinates[j][1] * scale + offsety)
+			pendown()
+			j+=1
+		setposition(coordinates[0][0] * scale + offsetx, coordinates[0][1] * scale + offsety)
+		penup()
+		i+=1
+
 main()
